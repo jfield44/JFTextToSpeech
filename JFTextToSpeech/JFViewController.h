f0ab5869af2ed3f07c894a9eebd7e625453cb24a
Jonathan Field <Jonathan@jonathans-mac-pro.local>
2013-09-25 19:18:47 +0100

    Initial Commit

--- JFTextToSpeech/JFViewController.h
+++ JFTextToSpeech/JFViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface JFViewController : UIViewController
+@interface JFViewController : UIViewController <AVSpeechSynthesizerDelegate, UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (nonatomic, retain) AVSpeechSynthesizer *speechSynthesizer;
+@property (weak, nonatomic) IBOutlet UISlider *speedSlider;
+
+- (IBAction)speakButtonWasPressed:(id)sender;
+- (void)speakText:(NSString *)toBeSpoken;
+- (IBAction)speechSpeedShouldChange:(id)sender;
 
 @end
--- JFTextToSpeech/JFViewController.m
+++ JFTextToSpeech/JFViewController.m
@@ -17,13 +17,69 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //Add Border to TextBox
+    [self.textView.layer setBorderWidth:.5f];
+    [self.textView.layer setBorderColor:[[UIColor blackColor] CGColor]];
+    [self.textView setDelegate:self];
+    
+    //Instantiate the object that will allow us to use text to speech
+     self.speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+    [self.speechSynthesizer setDelegate:self];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/*
+ Dismiss the keyboard when the user presses done
+ */
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    
+    BOOL retval = TRUE;
+    
+    if([text isEqualToString:@"\n"]){
+        [self.textView resignFirstResponder];
+        retval = FALSE;
+    }
+    
+    return retval;
+    
+}
+
+#pragma Speech Manipulation
+
+/*
+ User pressed the button titled 'Speak'
+ */
+- (IBAction)speakButtonWasPressed:(id)sender{
+
+    [self speakText:[self.textView text]];
+
+}
+
+/*
+ Take the current contents of the TextView and output it through the speakers of the user's device
+ */
+- (void)speakText:(NSString *)toBeSpoken{
+    
+    AVSpeechUtterance *utt = [AVSpeechUtterance speechUtteranceWithString:toBeSpoken];
+    utt.rate = [self.speedSlider value];
+    [self.speechSynthesizer speakUtterance:utt];
+    
+}
+
+/*
+ Increase or decrease the rate at which the text will be spoken
+ */
+- (IBAction)speechSpeedShouldChange:(id)sender
+{
+    UISlider *slider = (UISlider *)sender;
+    NSInteger val = lround(slider.value);
+    NSLog(@"%@",[NSString stringWithFormat:@"%ld",(long)val]);
 }
 
 @end
